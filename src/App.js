27188f4cdedeaa04cd18c3164902d21289e5630c
MdKaioumislam5 <afrankaiyum34@gmail.com>
2021-02-27 22:58:43 +0600

    work done

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function Counter() {
   
   return (
     <div>
-      <h1>Count: {count} </h1>
+      <h2>Count: {count} </h2>
       <button onMouseMove={() => setCount(count - 1)}>Decrease</button>
       <button onClick={() => setCount(count + 1)}>Increase</button>
     </div>
